d2cd926f4d6c8c0a3144e548c4444e78419f10b0
Artiom <artiom.bykovskih@gmail.com>
2025-08-04 20:02:35 +0300

    Initial commit

---
+++ app/src/main/java/hexlet/code/SearchEngine.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+
+public class SearchEngine {
+    public String getGreeting() {
+        return "Hey, you!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new SearchEngine().getGreeting());
+    }
+}
